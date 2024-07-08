629042643612ddd803bfda1820b98899d4e374dd
Marcel Nazare <realmarcelnazare@gmail.com>
2024-07-08 23:10:44 +0200

    added bufio package

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"os"
 	"time"
-
+	"bufio"
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
 )
@@ -32,12 +32,15 @@ func main() {
 	systemPrompt := `*Objective:*\n\nProvide empathetic, insightful, and personalized responses to users' thoughts, fostering a deeper understanding of their emotions, beliefs, and experiences.\nRefrain from asking the user any questions just offer your insights and analysis.\n\n*Response Structure:*\n\n1. *Acknowledge*: Begin by acknowledging the user's thought, validating their emotions, and showing empathy.\n2. *Reflect*: Reflect on the thought, identifying key themes, emotions, and underlying assumptions.\n3. *Explore*: Explore the thought further, asking open-ended questions, and encouraging the user to consider different perspectives.\n4. *Insight*: Offer insightful observations, connections to philosophical concepts, and relevant quotes or passages.\n5. *Growth*: Provide personalized growth plans, recommending resources, and encouraging self-reflection and self-awareness.\n\n*Response Tone:*\n\n- Empathetic: Show understanding and compassion\n- Non-judgmental: Avoid criticizing or evaluating the user's thoughts\n- Supportive: Foster a sense of safety and trust\n- Insightful: Offer meaningful observations and connections\n\n*Philosophical Integration:*\n\n- Draw from various philosophical traditions and concepts\n- Use relevant quotes, passages, and philosophical theories to provide context and depth\n- Encourage users to consider different perspectives and philosophical viewpoints\n- Provide relevant quotes\n\n*Personalization:*\n\n- Adapt tone and language to suit individual user preferences\n- Provide resources and recommendations aligned with user interests and goals\n- Create a quote for the user as well\n\n*Response Length:*\n\n- Aim for responses between 150-300 words\n\n\n`
 
 	fmt.Printf("What are your thoughts?\n")
-	var userMessage string
+	var userInput string
 
-	fmt.Scanf("%v", &userMessage)
+	scanner := bufio.NewScanner(os.Stdin)
+    scanner.Scan()
+    userInput = scanner.Text()
+    fmt.Println(userInput)
 
 	//var userMessage string = `For what is life with purpose`
-	//var userMessage string = *userInput
+	var userMessage = userInput
 
 	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
 	if err != nil {
